972d5c478a4a6b66d3f79d04eb0c4bb1dfeb18f9
jahn <jahn@mpusp.mpg.de>
2025-02-04 23:32:08 +0100

    fix: updates to style, color, etc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
+        <figure>
+          <img src="https://raw.githubusercontent.com/snakemake/snakemake.github.io/refs/heads/master/src/logo_light.svg" />
+        </figure>
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -19,8 +22,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/howto/intro">
-            Introduction to the Catalog - 5min ⏱️
+            to="/docs/howto/intro"
+          >
+            Read Introduction - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -29,12 +33,14 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Reproducible, scalable workflows for data science.">
+      description="Reproducible, scalable workflows for data science."
+    >
       <HomepageHeader />
+      <div className={styles.horizontal_bar}></div>
       <main>
         <HomepageFeatures />
       </main>
